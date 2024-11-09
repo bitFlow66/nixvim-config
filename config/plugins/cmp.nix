998a60270b3d83ea57a196f2c967fa24a8714b3d
bitFlow66 <63958223+bitFlow66@users.noreply.github.com>
2024-11-09 21:35:21 +0100

    Added jump to next snippet placeholder with Tab

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -16,9 +16,44 @@
         "<C-f>" = "cmp.mapping.scroll_docs(4)";
         "<C-Space>" = "cmp.mapping.complete()";
         "<C-e>" = "cmp.mapping.close()";
-        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        # "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        # "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        # "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<CR>" = "cmp.mapping(function(fallback)
+              if cmp.visible() then
+                  if require('luasnip').expandable() then
+                      require('luasnip').expand()
+                  else
+                      cmp.confirm({
+                          select = true,
+                      })
+                  end
+              else
+                  fallback()
+              end
+          end)";
+
+        # Go to next cmp entry or jump to next placeholder
+        "<Tab>" = "cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require('luasnip').locally_jumpable(1) then
+            require('luasnip').jump(1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' })";
+
+        # Go to previous cmp entry or jump to previous placeholder
+        "<S-Tab>" = "cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require('luasnip').locally_jumpable(-1) then
+            require('luasnip').jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' })";
       };
     };
   };

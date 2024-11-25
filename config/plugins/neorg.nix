2d1d130e782dc01d242abd5f5ba70969d80da763
bitFlow66 <63958223+bitFlow66@users.noreply.github.com>
2024-11-25 18:16:43 +0100

    Added base config for neorg

--- config/plugins/neorg.nix
+++ config/plugins/neorg.nix
@@ -2,5 +2,22 @@
   plugins.neorg = {
     enable = true;
     lazyLoading = true;
+    modules = {
+      "core.defaults" = {
+        # Activate module, but do not overwrite default config
+        __empty = null;
+      };
+      "core.concealer" = {
+        __empty = null;
+      };
+      "core.dirman" = {
+        config = {
+          workspaces = {
+            home = "~/notes/home";
+          };
+        };
+      };
+    };
   };
 }
+
